40c5eb81d91601175570f2d74d20d0f93d5ab30a
eddr6 <jainaditya400@gmail.com>
2020-10-07 11:41:47 +0530

    Network setup

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    public GameObject connectedScreen;
+    public GameObject disconnectedScreen;
+
+    public void OnConnectButton()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Master Connected.");
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+    }
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("In Lobby!");
+        disconnectedScreen.SetActive(false);
+        connectedScreen.SetActive(true);
+    }
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        connectedScreen.SetActive(false);
+        disconnectedScreen.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/RoomManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    public InputField createRoom;
+    public InputField joinRoom;
+    public void OnCreateRoomButton()
+    {
+        if (createRoom.text != "")
+        {
+            PhotonNetwork.CreateRoom(createRoom.text, new RoomOptions { MaxPlayers = 2 });
+        }
+        else
+        {
+            Debug.Log("No room id provided.");
+        }
+    }
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("Room created as: " + createRoom.text);
+       // PhotonNetwork.JoinRoom(createRoom.text);
+    }
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("in joined room!");
+        //PhotonNetwork.LeaveRoom();
+    }
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log(message);
+        createRoom.placeholder.GetComponent<Text>().text = "Room already exists...";
+        //((Text)(createRoom.placeholder)).text = "Room already exists...";
+    }
+}
