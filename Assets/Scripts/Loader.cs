509dda6cf441f37fd64d735419ad7e44e926b906
eddr6 <jainaditya400@gmail.com>
2020-10-12 22:56:42 +0530

    working network setup for 2 players

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Loader : MonoBehaviour
+{
+    public GameObject player;
+    void Start()
+    {
+        StartCoroutine(SpawnPlayer());
+    }
+
+    private IEnumerator SpawnPlayer()
+    {
+        yield return new WaitForSeconds(1);
+        PhotonNetwork.Instantiate(player.name, player.transform.position, player.transform.rotation);
+    }
+
+}
--- Assets/Scripts/RoomManager.cs
+++ Assets/Scripts/RoomManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
 using Photon.Realtime;
+using UnityEngine.SceneManagement;
 
 public class RoomManager : MonoBehaviourPunCallbacks
 {
@@ -17,23 +18,53 @@ public class RoomManager : MonoBehaviourPunCallbacks
         }
         else
         {
-            Debug.Log("No room id provided.");
+            Toast.Instance.Show("No room id provided.", 2f, Toast.ToastColor.Red);
+        }
+    }
+    public void OnJoinRoomButton()
+    {
+        if (joinRoom.text != "")
+        {
+            PhotonNetwork.JoinRoom(joinRoom.text);
+        }
+        else
+        {
+            Toast.Instance.Show("No room id provided.", 2f, Toast.ToastColor.Red);
         }
     }
     public override void OnCreatedRoom()
     {
         Debug.Log("Room created as: " + createRoom.text);
-       // PhotonNetwork.JoinRoom(createRoom.text);
+        Toast.Instance.Show("Room created as: " + createRoom.text, 3f, Toast.ToastColor.Blue);
+        // PhotonNetwork.JoinRoom(createRoom.text);
     }
     public override void OnJoinedRoom()
     {
         Debug.Log("in joined room!");
-        //PhotonNetwork.LeaveRoom();
+        Toast.Instance.Show("Room Joined!", 2f, Toast.ToastColor.Blue);
+        //FOR CLIENT TO LOAD THE LEVEL
+        if (PhotonNetwork.PlayerList.Length== 2)
+        {
+            Debug.Log("Players= "+PhotonNetwork.PlayerList.Length);
+            SceneManager.LoadScene("Level1");
+        }
+        
     }
     public override void OnCreateRoomFailed(short returnCode, string message)
     {
         Debug.Log(message);
-        createRoom.placeholder.GetComponent<Text>().text = "Room already exists...";
+        Toast.Instance.Show("Room already exists... " + createRoom.text, 2f, Toast.ToastColor.Red);
         //((Text)(createRoom.placeholder)).text = "Room already exists...";
+    }
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        Debug.Log(message);
+        Toast.Instance.Show(message, 2f, Toast.ToastColor.Red);
+    }
+    //Load the level FOR MASTER when second player (CLIENT) enters
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Toast.Instance.Show("New Player entered", 2f, Toast.ToastColor.Green);
+        SceneManager.LoadScene("Level1");
     }
 }
